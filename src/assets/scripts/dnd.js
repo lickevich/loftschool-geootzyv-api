c1d704e38ea3d596f1bf4bf5624aefec8e1a0994
Roman Lickevich <r.lickevich@gmail.com>
2019-03-24 22:43:53 +0200

    метка

--- src/assets/scripts/dnd.js
+++ src/assets/scripts/dnd.js
@@ -1,36 +1,39 @@
-const popup = document.querySelector('.geo-popup');
-const popupHeader = document.querySelector('.geo-popup__header');
+export default function dnd() {  
+  const popup = document.querySelector('.geo-popup');
+  const popupHeader = document.querySelector('.geo-popup__header');
 
-popupHeader.onmousedown = function(e) {
+  popupHeader.onmousedown = function(e) {
 
-    const coords = getCoords(popup);
-    const shiftX = e.pageX - coords.left;
-    const shiftY = e.pageY - coords.top;
+      const coords = getCoords(popup);
+      const shiftX = e.pageX - coords.left;
+      const shiftY = e.pageY - coords.top;
 
-    popup.style.position = 'absolute';
-    moveAt(e);
-  
-    popup.style.zIndex = 1000;
-  
-    function moveAt(e) {
-      popup.style.left = e.pageX - shiftX / 2 + 'px';
-      popup.style.top = e.pageY - shiftY / 2 + 'px';
-    }
-  
-    document.onmousemove = function(e) {
+      popup.style.position = 'absolute';
       moveAt(e);
-    }
-  
-    popup.onmouseup = function() {
-      document.onmousemove = null;
-      popup.onmouseup = null;
-    }
-}
+    
+      popup.style.zIndex = 1000;
+    
+      function moveAt(e) {
+        popup.style.left = e.pageX - shiftX + 'px';
+        popup.style.top = e.pageY - shiftY + 'px';
+      }
+    
+      document.onmousemove = function(e) {
+        moveAt(e);
+      }
+    
+      popup.onmouseup = function() {
+        document.onmousemove = null;
+        popup.onmouseup = null;
+      }
+  }
 
-function getCoords(elem) {
-    const box = elem.getBoundingClientRect();
-    return {
-      top: box.top + pageYOffset,
-      left: box.left + pageXOffset
-    };
+  function getCoords(elem) {
+      const box = elem.getBoundingClientRect();
+      return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+      };
+  }
 }
+dnd();
