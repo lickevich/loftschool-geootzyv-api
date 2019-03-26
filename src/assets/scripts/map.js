daefe8681c728078faf68dcd066e227c94d8dbc3
Roman Lickevich <r.lickevich@gmail.com>
2019-03-26 23:31:42 +0200

    добавил данные в localStorage

--- src/assets/scripts/map.js
+++ src/assets/scripts/map.js
@@ -1,43 +1,107 @@
-ymaps.ready(init);    
-export default function init(){ 
-    var myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 12
-    }, {
-        balloonMaxWidth: 200,
-        searchControlProvider: 'yandex#search'
-    });
-    // Обработка события, возникающего при щелчке
-    // левой кнопкой мыши в любой точке карты.
-    // При возникновении такого события откроем балун.
+const template = require('../templates/popup/comment.hbs');
+
+ymaps.ready(init);
+
+export default function init() {
+    const popup = document.querySelector('.geo-popup');
+    const popupClose = document.querySelector('.geo-popup__btn-close');
+    const sendButton = document.querySelector('.geo-popup__form__btn-send');
+    const nameInput = document.querySelector('#name-input');
+    const placeInput = document.querySelector('#place-input');
+    const textarea = document.querySelector('#text-textarea');
+    const commentList = document.querySelector('.geo-popup__comment-list');
+
+    let storage = localStorage;
+
+    var myPlacemark,
+        myMap = new ymaps.Map('map', {
+            center: [55.753994, 37.622093],
+            zoom: 12,
+            controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+
+    // Добавим на карту ползунок масштаба.    
+    myMap.controls.add('zoomControl');
+
+    // Слушаем клик на карте.
     myMap.events.add('click', function (e) {
-        if (!myMap.balloon.isOpen()) {
-            var coords = e.get('coords');
-            myMap.balloon.open(coords, {
-                contentHeader:'Событие!',
-                contentBody:'<p>Кто-то щелкнул по карте.</p>' +
-                    '<p>Координаты щелчка: ' + [
-                    coords[0].toPrecision(6),
-                    coords[1].toPrecision(6)
-                    ].join(', ') + '</p>',
-                contentFooter:'<sup>Щелкните еще раз</sup>'
-            });
-        }
-        else {
-            myMap.balloon.close();
-        }
+        
+
+
+        var coords = e.get('coords');
+
+        popup.style.position = 'absolute';
+        popup.classList.add('is-active');
+
+        // Создаем метку.
+        myPlacemark = createPlacemark(coords);
+        myMap.geoObjects.add(myPlacemark);
+        getAddress(coords);
+
+        console.log(storage.data);
     });
 
-    // Обработка события, возникающего при щелчке
-    // правой кнопки мыши в любой точке карты.
-    // При возникновении такого события покажем всплывающую подсказку
-    // в точке щелчка.
-    myMap.events.add('contextmenu', function (e) {
-        myMap.hint.open(e.get('coords'), 'Кто-то щелкнул правой кнопкой');
-    });
-    
-    // Скрываем хинт при открытии балуна.
-    myMap.events.add('balloonopen', function (e) {
-        myMap.hint.close();
-    }); 
+    // Создание метки.
+    function createPlacemark(coords) {
+        return new ymaps.Placemark(coords, {
+            iconCaption: 'поиск...'
+        }, {
+            preset: 'islands#redDotIconWithCaption',
+            draggable: false
+        });
+    }
+
+    // Определяем адрес по координатам (обратное геокодирование).
+    function getAddress(coords) {
+        myPlacemark.properties.set('iconCaption', 'поиск...');
+        ymaps.geocode(coords).then(function (res) {
+            var firstGeoObject = res.geoObjects.get(0);
+
+            myPlacemark.properties
+                .set({
+                    // Формируем строку с данными об объекте.
+                    iconCaption: [
+                        // Название населенного пункта или вышестоящее административно-территориальное образование.
+                        firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
+                        // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
+                        firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
+                    ].filter(Boolean).join(', '),
+                    // В качестве контента балуна задаем строку с адресом объекта.
+                    balloonContent: firstGeoObject.getAddressLine()
+                });
+        });
+    }
+
+    popupClose.addEventListener('click', () =>{
+        popup.classList.remove('is-active');
+    })
+
+    sendButton.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const date = new Date();
+        const day = date.getDay();
+        const mounth = date.getMonth();
+        const year = date.getFullYear();
+        const time = day + "." + mounth + "." + year;
+
+        storage.data = JSON.stringify({
+            list: [
+                {
+                    name: nameInput.value,
+                    place: placeInput.value,
+                    comment: textarea.value,
+                    date: time
+                }
+            ]
+        });
+
+        nameInput.value = '';
+        placeInput.value = '';
+        textarea.value = '';   
+    })
+
+    commentList.innerHTML = template(JSON.parse(localStorage.data));
 }
